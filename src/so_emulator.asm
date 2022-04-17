47eb324bdfaf8ad38e70cb56b3bf0fe3f6bc262d
Tima <timathevedmed@gmail.com>
2022-04-17 12:28:23 +0200

    Create constatnts

--- src/so_emulator.asm
+++ src/so_emulator.asm
@@ -1,6 +1,12 @@
 global so_emul
 
-section .text
+section .rodata
+mx_arg: dw      7      ; Maximal argument value
+arg1_c: dw      0x100  ; Coefficient for first argument
+arg2_c: dw      0x0800 ; Coefficient for second argument
+codes:  dw      0x0000, 0x4000
+labels: dq      mov, movi
 
+section .text
 so_emul:
         ret
