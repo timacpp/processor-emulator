5d816fe40169e7d1f695b6240e8926d132d2777e
Tima <timathevedmed@gmail.com>
2022-04-19 17:23:55 +0200

    Remove .rodata section

--- src/so_emulator.asm
+++ src/so_emulator.asm
@@ -1,12 +1,11 @@
 global so_emul
 
-section .rodata
-mx_arg: db      7      ; Maximal argument value
-mx_imm: db      255    ; Maximal immediate value
-arg1_c: dw      0x100  ; Coefficient for first argument
-arg2_c: dw      0x0800 ; Coefficient for second argument
-mov_c:  dw      0x0000 ; Coefficient for 'mov' instruction
-movi_c: dw      0x4000 ; Coefficient for 'movi' instruction
+MX_ARG  equ 0x7    ; Maximal argument value
+MX_IMM  equ 0xff   ; Maximal immediate value
+ARG1_C  equ 0x100  ; Coefficient for first argument
+ARG2_C  equ 0x0800 ; Coefficient for second argument
+MOV_C   equ 0x0000 ; Coefficient for 'mov' instruction
+MOVI_C  equ 0x4000 ; Coefficient for 'movi' instruction
 
 section .text
 ; Solution modifies rbx, rax, r8-15 registers and preserved rbx, r12-r15.
@@ -26,32 +25,33 @@ steps_loop: ; for (; steps > 0; steps--)
         ; TODO imm1 check
         xor     r14w, r14w ; Set loop variable for arg1_loop to zero
 arg1_loop: ;  for (uint16_t arg1 = 0; arg1 < mx_arg; arg1++)
-        mov     ax, [rel arg1_c]
+        mov     ax, ARG1_C
         mul     r14w
         mov     bx, ax ; bx = 0x100 * arg1
 
-        mov     ax, [rel mx_imm]
-        add     ax, [rel movi_c]
-        add     ax, bx
-        cmp     ax, r13w
+        mov     ax, r13w
+        sub     ax, bx
+        sub     ax, MOVI_C
+
+        cmp     ax, MX_IMM
         jna     movi
 
         xor     r15w, r15w ; Set loop variable for arg2_loop to zero
 arg2_loop: ;  for (uint16_t arg2 = 0; arg2 < mx_arg; arg2++)
-        mov     ax, [rel arg2_c]
+        mov     ax, ARG2_C
         mul     r15w
         add     bx, ax ; bx += 0x0800 * arg2
 
-        mov     ax, [rel mov_c]
+        mov     ax, MOV_C
         add     ax, bx
         cmp     ax, r13w
         je      mov
 
-        cmp     r15w, [rel mx_arg]
+        cmp     r15w, MX_ARG
         inc     r15w
         jna     arg2_loop
 ; arg2_loop end
-        cmp     r14w, [rel mx_arg]
+        cmp     r14w, MX_ARG
         inc     r14w
         jna     arg1_loop
 ; arg1_loop end
