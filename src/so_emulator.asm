2c533ae31572d876af21fb79e20b5e230348082f
Tima <timathevedmed@gmail.com>
2022-04-23 20:25:23 +0200

    Create template for instructions

--- src/so_emulator.asm
+++ src/so_emulator.asm
@@ -1,23 +1,56 @@
+; Solution modifies rax, rsp, rbx, rdi, rsi, r8 registers and preserves rbx.
+
 ; For the matter of simplicity I use term nibble to describe a single hexadecimal digit:
 ; 0x[highest nibble][second highest nibble][second lowest nibble][lowest nibble]
 
+; First, we introduce 5 instruction categories:
+; A2 - requires 2 arguments;
+; AI - requires 1 argument and 1 immediate value (in such order);
+; A1 - requires *only* one argument;
+; I1 - requires *only* one immediate value;
+; A0 - requires no parameters;
+
+; Highest nibble of an instruction encodes the category. Instruction type and
+; parameters, if such exist, are encoded differently for each category (more below).
+
 global so_emul
 
-; Maximal value of the highest nibble of instructions:
-A2_MAX  equ 0x3 ; - requiring two arguments
-AI_MAX  equ 0x6 ; - requiring one argument and immediate value
+; Maximal value of the highest nibble of instructions within the category:
+A2_HIMX equ 0x3
+AI_HIMX equ 0x6
 
-; Exact value of the highest nibble of instructions:
-A1_EXCT equ 0x7 ; - requiring only one argument
-I1_EXCT equ 0xC ; - requiring only one immediate value
+; Exact value of the highest nibble of instructions within the category:
+A1_HIEX equ 0x7
+I1_HIEX equ 0xC
 
-; Second highest nibbles of instructions without parameters
+; Second highest nibbles of instructions of each A0 instruction:
 CLC_2HI equ 0x0
 STC_2HI equ 0x1
 BRK_2HI equ 0xF
 
-; Second lowest nibbles of instructions with one argument
+; Second highest nibbles of instructions of each I1 instruction:
+JMP_2HI equ 0x0
+JNC_2HI equ 0x2
+JC_2HI  equ 0x3
+JNZ_2HI equ 0x4
+JZ_2HI  equ 0x5
+
+; Lowest nibbles of instructions of each A2 instruction:
+MOV_LO  equ 0x0
+OR_LO   equ 0x2
+ADD_LO  equ 0x4
+SUB_LO  equ 0x5
+ADC_LO  equ 0x6
+SBB_LO  equ 0x7
+
+; Second lowest nibbles of each A1 instruction:
 RCR_2LO equ 0x0
+
+; Maximal highest *byte* of each A2 instruction:
+MOVI_BY equ 0x47
+XORI_BY equ 0x5F
+ADDI_BY equ 0x67
+CMPI_BY equ 0x6F
 
 ; Bytes required to store a CPU state
 CPU_SZ  equ 8
@@ -32,17 +65,20 @@ C_FLAG  equ 6
 Z_FLAG  equ 7
 
 section .text
+match_arg:
+        ret
 
-so_emul:
+so_emul: ; TODO: preserve state
+        xor     rax, rax
         test    rdx, rdx    ; If program consists of 0 steps
         jz      end_program ; Leave CPU state unmodified and end program
 
         push    rbx              ; Preserve nonvolatile rbx register
         xor     r8, r8           ; Clear temporary variable for future use
         sub     rsp, CPU_SZ      ; Allocate 8 bytes on stack for CPU state
-        mov     qword [rsp], rax ; Use previous CPU state parameters
+        mov     qword [rsp], 0
 
-steps_loop: ; for (; steps > 0; steps--)
+steps_loop:
         mov     r8b, byte [rsp + PC_CNT] ; Read index of the current instruction
         mov     ax, word [rdi + r8 * 2]  ; Read code of current instruction
 
@@ -54,54 +90,133 @@ steps_loop: ; for (; steps > 0; steps--)
 
         and     ah, 0x0F ; Extract second highest nibble
         and     al, 0xF0 ; Extract second lowest nibble
-instructions_two_args:
-        cmp     bh, A2_MAX
-        ja      instructions_arg_imm8
-        ; TODO
 
-instructions_arg_imm8:
-        cmp     bh, AI_MAX
-        ja      instructions_one_arg
-        ; TODO
+        ; Now instruction code can be represented as 0x[bh][ah][al][bl].
 
-instructions_one_arg:
+; Check if instruction is from A2 and if so, execute it.
+execute_a2:
+        cmp     bh, A2_HIMX ; If the highest nibble is not from range [0x0, 0x3]
+        ja      execute_ai  ; then the instruction does not belong to A2.
+
+.mov:
+        cmp     bl, MOV_LO
+        jne     .or
+        ; TODO
+        jmp     executed
+.or:
+        cmp     bl, OR_LO
+        jne     .add
+        ; TODO
+        jmp     executed
+.add:
+        cmp     bl, ADD_LO
+        jne     .sub
+        ; TODO
+        jmp     executed
+.sub:
+        cmp     bl, SUB_LO
+        jne     .adc
+        ; TODO
+        jmp     executed
+.adc:
+        cmp     bl, ADC_LO
+        jne     .sbb
+        ; TODO
+        jmp     executed
+.sbb:
+        cmp     bl, SBB_LO
+        jne     execute_ai
+        ; TODO
+        jmp     executed
+
+; Check if instruction is from AI and if so, execute it.
+execute_ai:
+        cmp     bh, AI_HIMX ; If the highest nibble is not from range [0x4, 0x6]
+        ja      execute_a1  ; then the instructions does not belong to AI.
+
+        mov     bl, ah ; Now bx holds the highest byte of the code
+.movi:
+        cmp     bx, MOVI_BY
+        jne     .xori
+        ; TODO
+        ja    executed
+.xori:
+        cmp     bx, XORI_BY
+        ja      .addi
+        ; TODO
+        jmp      executed
+.addi:
+        cmp     bx, ADDI_BY
+        ja      .cmpi
+        ; TODO
+        jmp     executed
+.cmpi:
+        cmp     bx, CMPI_BY
+        ja      execute_a1
+        ; TODO
+        jmp     executed
+
+; Check if instruction is from A1 and if so, execute it.
+execute_a1:
+        cmp     bh, A1_HIEX ; If the highest nibble is not equal to 0x7
+        jne     execute_i1  ; then the instruction does not belong to A1.
 ; Argument value is encoded on the second highest nibble.
-        cmp     bh, A1_EXCT
-        jne     instructions_one_imm8
-rcr:
+.rcr:
         cmp     al, RCR_2LO
         jne     executed
-        ; TODO: RCR
+        ; TODO
+        jmp     executed ; Ignore unmatched instruction
+
+execute_i1:
+        cmp     bh, I1_HIEX ; If the highest nibble is not equal to 0xC
+        jne     execute_a0  ; then the instruction does not belong to I1
+.jmp:
+        cmp     ah, JMP_2HI
+        jne     .jnc
+        ; TODO
+        jmp     executed
+.jnc:
+        cmp     ah, JNC_2HI
+        jne     .jc
+        ; TODO
+        jmp     executed
+.jc:
+        cmp     ah, JC_2HI
+        jne     .jnz
+        ; TODO
+        jmp     executed
+.jnz:
+        cmp     ah, JNZ_2HI
+        jne     .jz
+        ; TODO
+        jmp     executed
+.jz:
+        cmp     ah, JZ_2HI
+        jne     execute_a0
+        ; TODO
         jmp     executed
 
-instructions_one_imm8:
-        cmp     bh, I1_EXCT
-        jne     instructions_no_args
-
-
-instructions_no_args:
-; Instruction type is encoded on the second highest nibble.
-clc:
-        cmp     ah, CLC_2HI ; 0x8[0]00
-        jne     stc
+; *Assume* instruction is from A0 and *try* to execute it.
+execute_a0:
+.clc:
+        cmp     ah, CLC_2HI
+        jne     .stc
         mov     byte [rsp + C_FLAG], 0 ; Clear virtual carry factor
         jmp     executed
-
-stc:
-        cmp     ah, STC_2HI ; 0x8[1]00
-        jne     brk
+.stc:
+        cmp     ah, STC_2HI
+        jne     .brk
         mov     byte [rsp + C_FLAG], 1 ; Set virtual carry factor
         jmp     executed
-
-brk:
-        cmp     ah, BRK_2HI ; 0xF[F]FF
+.brk:
+        cmp     ah, BRK_2HI
         jne     executed    ; Ignore unmatched instruction
-        jmp     build_state ; Break from the program
+        jmp     build_state ; Terminate the program
 
 executed:
-        inc     byte [rsp + PC_CNT]
-        dec     rdx
-        jnz     steps_loop
+        inc     byte [rsp + PC_CNT] ; Increment PC counter
+        dec     rdx                 ; Decrement steps left to perform
+        jnz     steps_loop          ; Repeat steps_loop untill steps is zero
 
 build_state:
         mov    rax, qword [rsp]
